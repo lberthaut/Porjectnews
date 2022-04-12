6209395c15a03c9bce5a97f64c273939700f5021
Louis Berthaut <louis.berthaut@gmx.com>
2022-04-12 16:42:18 +0200

    add sort

--- js/getarticles.js
+++ js/getarticles.js
@@ -1,4 +1,28 @@
-const url = 'https://newsapi.org/v2/top-headlines?country=fr&apiKey=fc0456071e5e43499f389f5b2577213f';
+var url = 'https://newsapi.org/v2/top-headlines?country=fr&apiKey=fc0456071e5e43499f389f5b2577213f';
+const mainCtn = document.getElementById('articles_container');
+const selections = document.querySelectorAll('.menu_selection');
+for (const selection of selections) {
+    selection.addEventListener('click', () => {
+        mainCtn.innerHTML = "";
+        var keySelection = selection.outerText;
+        var url = `https://newsapi.org/v2/everything?` +
+            `q=${keySelection}&` +
+            'sortBy=publishedAt&' +
+            'language=fr&' +
+            'searchIn=title&' +
+            'apiKey=fc0456071e5e43499f389f5b2577213f';
+        fetch(url)
+            .then((resp) => resp.json())
+            .then(function (data) {
+                let articles = data.articles;
+                articles.map(article => MakeArticle(article))
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    })
+}
+
 
 fetch(url)
     .then((resp) => resp.json())
@@ -10,9 +34,9 @@ fetch(url)
         console.log(error);
     });
 
+
 function MakeArticle(articles) {
     var dateFr = new Date(articles.publishedAt).toLocaleString();
-    const mainCtn = document.getElementById('articles_container')
     mainCtn.innerHTML += `
         <div class="article_tile">
         <a class="article_link" href="${articles.url}">
@@ -24,5 +48,4 @@ function MakeArticle(articles) {
         <p class="article_published">${dateFr}</p>
         </a>
         </div>`
-
 }
